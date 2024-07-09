cb0fb5d7150634c7f9664337c0dfddc5cd3d2875
Purva Chopdekar <purva@Purvas-Laptop.local>
2024-07-09 19:15:52 +0530

    Auth Page

--- lib/login_page_model.dart
+++ lib/login_page_model.dart
@@ -2,44 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-// import '/Auth/LoginScreen.dart' show LoginScreen;
 import 'Auth/loginAuth.dart';
 
-
-//Auth
-class login_page_model extends State<LoginScreen> {
-  //  State fields for stateful widgets in this page.
-  // State field(s) for emailAddress-login widget.
+class LoginPageModel extends State<LoginScreen> {
   FocusNode? emailAddressLoginFocusNode;
   TextEditingController? emailAddressLoginController;
   String? Function(BuildContext, String?)? emailAddressLoginControllerValidator;
-  // State field(s) for password-login widget.
+  
   FocusNode? passwordLoginFocusNode;
   TextEditingController? passwordLoginController;
   late bool passwordLoginVisibility;
   String? Function(BuildContext, String?)? passwordLoginControllerValidator;
 
-  /// Initialization and disposal methods.
+//FocusNode? confirmPasswordFocusNode;
+//TextEditingController? confirmPasswordController;
+//late bool confirmPasswordVisibility;
+//String? Function(BuildContext, String?)? confirmPasswordControllerValidator;
 
-  // void initState(BuildContext context) {
+// void initState(BuildContext context) {
   //   passwordLoginVisibility = false;
   // }
 
   void dispose() {
     emailAddressLoginFocusNode?.dispose();
     emailAddressLoginController?.dispose();
-
     passwordLoginFocusNode?.dispose();
     passwordLoginController?.dispose();
+    //super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     throw UnimplementedError();
   }
-
-  /// Action blocks are added here.
-
-  /// Additional helper methods are added here.
 }
